388d7ec739872442e217e643744a973e6545a958
Aditya Jambhale <jambhaleaditya91@gmail.com>
2024-12-29 14:58:02 +0530

    completed

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,19 +1,15 @@
-"use client";
+import { FC } from "react";
 
-import React from "react";
-
-// Update PageProps to reflect the correct type
-interface PageProps {
-  params: {
-    id: string;
-  };
+interface ProfilePageProps {
+    params: {
+        id: string;
+    };
 }
 
-export default function ProfilePage({ params }: PageProps) {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1>Profile Page</h1>
-      <h2 className="bg-green-300 text-black">{params.id}</h2>
-    </div>
-  );
-}
+const ProfilePage: FC<ProfilePageProps> = ({ params }) => {
+    const { id } = params;
+
+    return <div>Profile ID: {id}</div>;
+};
+
+export default ProfilePage;
